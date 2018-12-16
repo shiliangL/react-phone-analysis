4f1eb24611be4cebe4607a1db5fb38cff836c0ef
shiliangl <shiliangwei@cloudolp.com>
2018-12-16 17:40:39 +0800

    打包测试

---
+++ build/precache-manifest.55a4890cf427bf38229509de638580d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "640ab3eda51139757caf",
+    "url": "/static/css/main.a6f20cb3.chunk.css"
+  },
+  {
+    "revision": "640ab3eda51139757caf",
+    "url": "/static/js/main.640ab3ed.chunk.js"
+  },
+  {
+    "revision": "7cd23b09c0830c9eeb26",
+    "url": "/static/css/1.c2ff7451.chunk.css"
+  },
+  {
+    "revision": "7cd23b09c0830c9eeb26",
+    "url": "/static/js/1.7cd23b09.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e91b9f0616e66040a1075a2d924c7de",
+    "url": "/static/media/iconfont.1e91b9f0.eot"
+  },
+  {
+    "revision": "db418169d48dec9007fc23a694bf0c99",
+    "url": "/static/media/iconfont.db418169.ttf"
+  },
+  {
+    "revision": "912c136451fae8a510fd1438110f130f",
+    "url": "/static/media/iconfont.912c1364.svg"
+  },
+  {
+    "revision": "e64243788967e5723af00a49262ce540",
+    "url": "/index.html"
+  }
+];
